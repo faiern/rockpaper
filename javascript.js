14be361b6d3bc37eeb2e4cc6c112e89e3879e3a7
faiern <thomasl.tlouis@gmail.com>
2023-01-21 17:35:47 +0100

    changes are made

--- javascript.js
+++ javascript.js
@@ -18,26 +18,46 @@ function playRound(playerSelection,computerSelection){
     {
         let response = "Well played, you won because you chose : " + translatePlayer + " and the computer chose : " + translateComputer;
         console.log(response);
+        return response;
         
     }
-    else if (translatePlayer == translateComputer){
+    else if (translatePlayer === translateComputer){
         let response = translatePlayer +" vs "+ translateComputer +" That's a Tie ! ";
         console.log(response);
+        return response;
     }
     else{
         let response = " You lost " + translateComputer + " beats " + translatePlayer;
         console.log(response);
+        return response;
     }
 }
  
 
-function game() {
-    let response = prompt("What's your choice ?");
+//function game() {
+    //let response = prompt("What's your choice ?");
       
-    playRound(response,getComputerChoice());
+    //playRound(response,getComputerChoice());
     
-        
-    
-}
+//}
 
-console.log(game());
+//console.log(game());
+
+const answer = document.createElement("div");
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+
+buttons.forEach((item)=>{
+    item.addEventListener("click",() => {
+    playRound(item.className,getComputerChoice());
+    console.log(item.className);
+    answer.textContent = playRound(item.className,getComputerChoice());
+    });
+})
+
+const container = document.getElementById("container");
+container.appendChild(answer);
+
+
+
+
